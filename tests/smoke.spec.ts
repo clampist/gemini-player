372e01bad19c8611ab730d0c97baeba7f5ab16d2
clampist <1221709+clampist@users.noreply.github.com>
2025-11-10 20:08:34 +0900

    fix smoke

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { GeminiChatPage } from './pages/GeminiChatPage';
 import { GeminiSearchPage } from './pages/GeminiSearchPage';
 import { GeminiSettingsPage } from './pages/GeminiSettingsPage';
